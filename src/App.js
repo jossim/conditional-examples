f4a32d8de923cb38522b0f9f2eaf2ff7043daa1e
Joseph Simmons <josephdsimmons@gmail.com>
2021-09-02 09:47:58 -0500

    element variable example

--- src/App.js
+++ src/App.js
@@ -1,29 +1,79 @@
-import React from "react";
+import React, {Component} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function UserGreeting(props) {
-  return <h1>Welcome back!</h1>;
+function UserGreeting(props){
+  return <h1>Welcome back!</h1>
 }
 
-function GuestGreeting(props) {
-  return <h1>Please sign up.</h1>;
+function GuestGreeting(props){
+  return <h1>Please sign up.</h1>
 }
 
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-
+function Greeting(props){
+  const isLoggedIn = props.isLoggedIn
   if (isLoggedIn) {
-    return <UserGreeting />;
+    return <UserGreeting />
+  }
+  return <GuestGreeting />
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  )
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  )
+}
+
+class LoginControl extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false
+    }
   }
 
-  return <GuestGreeting />;
+  handleLoginClick = () => {
+    this.setState({ isLoggedIn: true })
+  }
+
+  handleLogoutClick = () => {
+    this.setState({ isLoggedIn: false })
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      <Greeting isLoggedIn={false} />
+          <LoginControl /> 
     </div>
   );
 }
